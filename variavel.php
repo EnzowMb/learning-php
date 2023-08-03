23fdfe1a633729a8a11852f3389ac9cce876641a
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-08-03 17:28:42 -0300

    aprendendo variavel

--- variavel.php
+++ variavel.php
@@ -1,7 +1,19 @@
 <?php
 
-$idade = 21;
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
 
-$idadeDaqui10anos = $idade + 10;
+$doisAoCubo = 2 ** 3;
 
-echo $idadeDaqui10anos;
+$restoDaDivisão = 10 % 3;
+
+$salario = 1000.3012;
+
+$texto = "Olá mundo";
+
+$verdadeiro = true;
+$false = false;
+
+echo gettype($false);
