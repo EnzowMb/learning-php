72951e103d5677bb171f7f40d91ffed55eac9167
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-08-03 17:44:48 -0300

    quebra de linha

--- string.php
+++ string.php
@@ -2,6 +2,12 @@
 
 $idade = 21;
 
-echo "Olá mundo!";
+echo "Olá mundo!\n";
 
-echo "Minha idade é $idade anos";
+echo "\t";
+
+echo "Minha idade é $idade anos" . PHP_EOL;
+// PHP_EOL Garante q a quabra de linha vai funcionar em qualquer sistema operacional
+// Isso porque em alguns sistemas operacionais utilizamos \r, outros \n, etc
+
+echo "Olá";
